3b3509b6a34a1a6f9883d1b00b91c90fe7bec4e2
fafalai <fafalai@users.noreply.github.com>
2018-05-15 14:26:40 +1000

    menu, banner

--- bottom.php
+++ bottom.php
@@ -1,3 +1,4 @@
   <div id="footer">
-    <p>Copyright &copy; 2017 <a href="http://www.adtalk.com.au" target="_blank">Adtalk Pty Ltd.</a> All Rights Reserved.</p>
+    <!-- <p>Copyright &copy; 2017 <a href="http://www.adtalk.com.au" target="_blank">Adtalk Pty Ltd.</a> All Rights Reserved.</p> -->
+    <p>Copyright &copy; 2018 Adverdent Pty Ltd. Created by Adtalk Pty Ltd. All Rights Reserved.</p>
   </div>
--- top.php
+++ top.php
@@ -5,81 +5,67 @@
       }
 
 </script>
-<div>
-    <table>
-      <tr>
-        <td align="left" valign="top">
-          <?php
-            if (SharedIsLoggedIn())
-            {
-              echo $_SESSION['username'];
-            }
-            else
-            {
-              echo "&nbsp;";
-            }
-          ?>
-        </td>
-        <td align="left" valign="top"><img src="images/tint-app-logo.png" width="110" height="70" alt="Tinting Logo" longdesc="http://www.remedyappserver.com"></td>
-      </tr>
-    </table>
-  </div>
+<div style="padding: 10px;margin-bottom: 80px">
+  <table style="background-color:white;padding: 20px">
+          <tr>
+            <td align="right" valign="top"><img src="images/tint-app-logo.png" width="220" height="100" alt="Tinting Logo" longdesc="http://www.remedyappserver.com" style="margin-left: 100px;padding: 10px"></td>
+            <?php
+              if (!SharedIsLoggedIn())
+              {
+              ?>
+                <td>
+                  <button id="login" onclick="redirect('login.php')" style="background-image: url('login_btn.png');width: 125px;height: 39px;cursor: pointer;font-size: 12pt;color: white;font-weight: bold;margin-left: 200px">Log in</button>
+                </td>
+                <td>
+                  <button id="signup" onclick="redirect('signu.php')" style="background-image: url('signup_btn.png');width: 125px;height: 39px;cursor: pointer;font-size: 12pt;color: white;font-weight: bold">&nbsp;&nbsp;&nbsp;Sign up</button>
+                </td>
+              <?php
+              }
+              else
+              {
+                echo "&nbsp;";
+              }
+              ?>
+
+            <td>
+              <div id="menu">
+                <ul>
+                    <?php
+                      if (SharedIsLoggedIn())
+                      {
+                    ?>
+                        <li class="current_page_item"><a href="index.php" class="first">Home</a></li>
+                        <li><a href="logout.php">Logout</a></li>
+                        <li><a href="jobs.php">Jobs</a></li>
+                      <?php
+                        if ($_SESSION['admin'] != 0)
+                        {
+                      ?>
+                        <!-- <li><a href="tagreps.php">Reports</a></li> -->
+                       
+                        <li><a href="clients.php">Clients</a></li>
+                        <li><a href="users.php">Users</a></li>
+                        <li><a href="profile.php">Company</a></li>
+                        <li><a href="resource.php">Resource</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                        <li><a href="">Account</a></li>
+                      <?php
+                        }
+                      ?>
+                  <?php
+                      }
+                      else
+                      {
+                        echo "&nbsp;";
+                      }
+                      ?>     
+                </ul>
+              </div>
+            </td>
+          </tr>
+  </table>
 </div>
-<div id="header">
-    <?php
-      if (!SharedIsLoggedIn())
-      {
-      ?>
-         <button id="login" onclick="redirect('login.php')" style="background-image: url('login_btn.png');width: 125px;height: 39px;cursor: pointer;font-size: 12pt;color: white;font-weight: bold">Log in</button>
-         <button id="signup" onclick="redirect('signu.php')" style="background-image: url('signup_btn.png');width: 125px;height: 39px;cursor: pointer;font-size: 12pt;color: white;font-weight: bold">Sign up</button>
-      <?php
-      }
-      else
-      {
-        echo "&nbsp;";
-      }
-    ?>
-  <div id="menu">
-    <ul>
-      <li class="current_page_item"><a href="index.php" class="first">Home</a></li>
-      <?php
-        if (SharedIsLoggedIn())
-        {
-      ?>
-          <li><a href="logout.php">Logout</a></li>
-      <?php
-        }
-        else
-        {
-      ?>
-          <li><a href="login.php">Login</a></li>
-          <li><a href="signup.php">Sign Up</a></li>
-      <?php
-        }
-      ?>
-      <li><a href="contact.php">Contact</a></li>
-      <?php
-        if (SharedIsLoggedIn())
-        {
-      ?>
-          <li><a href="jobs.php">Jobs</a></li>
-      <?php
-          if ($_SESSION['admin'] != 0)
-          {
-      ?>
-            <!-- <li><a href="tagreps.php">Reports</a></li> -->
-            <li><a href="clients.php">Clients</a></li>
-            <li><a href="users.php">Users</a></li>
-            <li><a href="profile.php">Company</a></li>
-            <li><a href="resource.php">Resource</a></li>
 
-      <?php
-          }
-        }
-      ?>      
-    </ul>
-  </div>
-
-<div id="logo">
+<!-- <div id="logo">
   <h1><img src="images/tint-app-logo.png" width="150" height="80"></h1>
-</div>
+</div> -->

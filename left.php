b4043f6023bed207aefe2811dad2ae68e5496ed4
Akhilesh <c1akhileshlamba02@gmail.com>
2018-05-16 17:26:03 +1000

    update left menu

--- left.php
+++ left.php
@@ -67,42 +67,51 @@
         {
         ?>
                 <li>
-                    <div id="DIV_FBContainer">
-                        <div id="DIV_FeaturesBanner">
-                            <strong>Click here to sign up today for your <font color="red">FREE</font> 30 Day trial</strong>
+                    <div id="Container" class="container text-center">
+                        <div id="TitleBanner">
+                            <Strong>Click here to sign up today for your <font color='red'>FREE</font> 30 Day trial</Strong>
                         </div>
-                        <!--                        <div id="FBcenter">-->
-                        <div id="DIV_featuresContainer">
-                            <div>
-                                <h2>Features</h2>
+                        <div class="row" style="margin: auto;">
+                            <div id="DIV_FContainer" class="col">
+                                <div id="DIV_FTitle">
+                                    <Strong>FEATURES</Strong>
+                                </div>
+                                <div>
+                                    <ul>
+                                        <li><a href="#">iPhone&reg; / iPad&reg; App</a></li>
+                                        <li><a href="#">Easy to setup and use</a></li>
+                                        <li><a href="#">Auto populated fields</a></li>
+                                        <li><a href="#">Barcode & QR scanning options</a></li>
+                                        <li><a href="#">Camera capabilities</a></li>
+                                        <li><a href="#">Automatic reporting</a></li>
+                                        <li><a href="#">Location maps</a></li>
+                                        <li><a href="#">Job tracking</a></li>
+                                    </ul>
+                                </div>
                             </div>
-                            <ul>
-                                <li><a href="#">iPhone&reg; / iPad&reg; App</a></li>
-                                <li><a href="#">Easy to setup and use</a></li>
-                                <li><a href="#">Auto populated fields</a></li>
-                                <li><a href="#">Barcode & QR scanning options</a></li>
-                                <li><a href="#">Camera capabilities</a></li>
-                                <li><a href="#">Automatic reporting</a></li>
-                                <li><a href="#">Location maps</a></li>
-                                <li><a href="#">Job tracking</a></li>
-                            </ul>
-                        </div>
-                        <div id="DIV_benefitsContainer">
-                            <div>
-                                <h2>Features</h2>
+                            <div id="DIV_BContainer" class="col">
+                                <div id="DIV_BTitle">
+                                    <Strong>BENEFITS</Strong>
+                                </div>
+                                <div>
+                                    <ul>
+                                        <li><a href="#">iPhone&reg; / iPad&reg; App</a></li>
+                                        <li><a href="#">Easy to setup and use</a></li>
+                                        <li><a href="#">Auto populated fields</a></li>
+                                        <li><a href="#">Barcode & QR scanning options</a></li>
+                                        <li><a href="#">Camera capabilities</a></li>
+                                        <li><a href="#">Automatic reporting</a></li>
+                                        <li><a href="#">Location maps</a></li>
+                                        <li><a href="#">Job tracking</a></li>
+                                    </ul>
+                                </div>
                             </div>
-                            <ul>
-                                <li><a href="#">Real-time data capture</a></li>
-                                <li><a href="#">Job register</a></li>
-                                <li><a href="#">Centralised reporting</a></li>
-                                <li><a href="#">Automatic reminders</a></li>
-                                <li><a href="#">Accurate data</a></li>
-                                <li><a href="#">Quotes automatically timestamped</a></li>
-                                <li><a href="#">Quotes are location aware</a></li>
-                            </ul>
                         </div>
-                        <!--                        </div>-->
+                        <div id="DIV_Message">
+                            <p>Want to get in touch? <a href="#">Click here</a> to message us.</p>
+                        </div>
                     </div>
+
                     <!--
 <h2>Welcome</h2>
 <p><a href="login.php">Sign up</a> today and choose from our flexible payment options!</p>

5fddc6a2ade9da25b793ad9b5624fd7af84a0f57
Akhilesh <c1akhileshlamba02@gmail.com>
2018-05-22 15:07:56 +1000

    Fix menu link display

--- top.php
+++ top.php
@@ -1,12 +1,15 @@
+<!--
 <head>
-    <!-- <link rel="stylesheet" type="type/css" href="styles.css"> -->
+     <link rel="stylesheet" type="type/css" href="styles.css">
     <script type="text/javascript">
         function redirect(address) {
             location.href = address;
         }
 
     </script>
+
 </head>
+-->
 
 <?php
 //              if (SharedIsLoggedIn())
@@ -52,12 +55,12 @@
                         {
                       ?>
                                 <!-- <li><a href="tagreps.php">Reports</a></li> -->
-                                <li class="current_page_item"><a href="index.php" class="first">Home</a></li>
+                                <li><a href="index.php">Home</a></li>
                                 <li><a href="clients.php">Clients</a></li>
                                 <li><a href="resource.php">Resource</a></li>
                                 <li><a href="contact.php">Contact</a></li>
                                 <div class="dropdown">
-                                    <li><a style="cursor: pointer">Account</a></li>
+                                    <li><a href="#">Account</a></li>
                                     <!-- <li><button style="border:none"></button>Account</button></li> -->
                                     <div class="dropdown-content">
                                         <a href="profile.php">Business Details</a>
@@ -78,4 +81,4 @@
                 </div>
 
     </div>
-
+    <script src="js/general.js"></script>

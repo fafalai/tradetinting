7b3dcbead9c871329a023afee96f233dd0f85816
Akhilesh <c1akhileshlamba02@gmail.com>
2018-05-23 10:01:02 +1000

    Version 2

--- feature.php
+++ feature.php
@@ -12,7 +12,7 @@
                     <li><a href="#">iPhone&reg; / iPad&reg; App</a></li>
                     <li><a href="#">Easy to setup and use</a></li>
                     <li><a href="#">Auto populated fields</a></li>
-                    <li><a href="#">Barcode & QR scanning options</a></li>
+<!--                    <li><a href="#">Barcode & QR scanning options</a></li>-->
                     <li><a href="#">Camera capabilities</a></li>
                     <li><a href="#">Automatic reporting</a></li>
                     <li><a href="#">Location maps</a></li>
@@ -26,14 +26,12 @@
             </div>
             <div>
                 <ul>
-                    <li><a href="#">iPhone&reg; / iPad&reg; App</a></li>
-                    <li><a href="#">Easy to setup and use</a></li>
-                    <li><a href="#">Auto populated fields</a></li>
-                    <li><a href="#">Barcode & QR scanning options</a></li>
-                    <li><a href="#">Camera capabilities</a></li>
-                    <li><a href="#">Automatic reporting</a></li>
-                    <li><a href="#">Location maps</a></li>
-                    <li><a href="#">Job tracking</a></li>
+                    <li><a href="#">Save time</a></li>
+                    <li><a href="#">Single entry</a></li>
+                    <li><a href="#">Centralised reporting</a></li>
+                    <li><a href="#">Improves close rate</a></li>
+                    <li><a href="#">Quotes automatically timestamped</a></li>
+                    <li><a href="#">Quotes are location aware</a></li>
                 </ul>
             </div>
         </div>

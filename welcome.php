5bbfc14be9e477f787dcbec5bd8538b449a3b206
fafalai <fafalai@users.noreply.github.com>
2018-05-23 11:41:45 +1000

    welcome page

--- welcome.php
+++ welcome.php
@@ -10,14 +10,17 @@
 <body>
     <div class="introducingDIV">
             <div class="sectionDIV">
-                    <span style="font-size: 24pt;font-weight: bold;">
-                            WELCOME  <?php echo $_SESSION['username'];?> ! What would you like to do? 
-                    </span>
-                    <br/>
-                    <button  onclick="redirect('jobs.php')" class="sectionButton">VIEW / ADD JOBS</button>
-                    <button  onclick="redirect('clients.php')" class="sectionButton">VIEW / ADD CLIENTS</button>
-                    <button  onclick="redirect('resource.php')" class="sectionButton">VIEW RESOURCES</button>
-                    <button  onclick="redirect('contact.php')" class="sectionButton">CONTACT</button>
+                <span style="font-size: 24pt;font-weight: bold;">
+                            WELCOME  <?php echo $_SESSION['username'];?> ! 
+                </span>
+                <span style="font-size: 22pt;font-style: italic;">
+                What would you like to do? 
+                </span>
+                <br/>
+                <button  onclick="redirect('jobs.php')" class="sectionButton">VIEW / ADD JOBS</button>
+                <button  onclick="redirect('clients.php')" class="sectionButton">VIEW / ADD CLIENTS</button>
+                <button  onclick="redirect('resource.php')" class="sectionButton">VIEW RESOURCES</button>
+                <button  onclick="redirect('contact.php')" class="sectionButton">CONTACT</button>
             </div>
     </div>
 </body>
